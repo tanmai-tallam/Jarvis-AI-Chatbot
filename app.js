c7c45bac771723fbc11891f52500c21693ae71ab
Tanmai Tallam <tanmai.tallam@gmail.com>
2024-06-13 16:54:43 +0530

    commit changes

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ function wishMe() {
     var hour = day.getHours();
 
     if (hour >= 0 && hour < 12) {
-        speak("Good Morning Boss...");
+        speak("Good Morning...");
     } else if (hour >= 12 && hour < 17) {
-        speak("Good Afternoon Master...");
+        speak("Good Afternoon...");
     } else {
-        speak("Good Evening Sir...");
+        speak("Good Evening...");
     }
 }
 
 window.addEventListener('load', () => {
-    speak("Initializing JARVIS...");
+    speak("Initializing NEOBOT...");
     wishMe();
 });
 
