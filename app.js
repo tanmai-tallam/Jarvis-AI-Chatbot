61afcf8b9fd56b5f93471ceaddcf489d7bf7911b
Tanmai Tallam <tanmai.tallam@gmail.com>
2024-06-18 20:57:34 +0530

    update commit

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ btn.addEventListener('click', () => {
 function takeCommand(message) {
     if (message.includes('hi') || message.includes('hello')) {
         speak("Hello , How May I Help You?");
+    } else if (message.includes("how are you")) {
+        speak("I am doing good , How about you?...");
+    } else if (message.includes("i love you")) {
+        speak("I love you too , How you doing?...");
+    } else if (message.includes('song') || message.includes('songs') ) {
+        window.open("https://open.spotify.com/", "_blank");
+        speak("Opening spotify...");
     } else if (message.includes("open google")) {
         window.open("https://google.com", "_blank");
         speak("Opening Google...");
@@ -56,10 +63,6 @@ function takeCommand(message) {
     } else if (message.includes("open facebook")) {
         window.open("https://facebook.com", "_blank");
         speak("Opening Facebook...");
-    } else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
-        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
-        const finalText = "This is what I found on the internet regarding " + message;
-        speak(finalText);
     } else if (message.includes('wikipedia')) {
         window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "").trim()}`, "_blank");
         const finalText = "This is what I found on Wikipedia regarding " + message;
@@ -75,6 +78,10 @@ function takeCommand(message) {
     } else if (message.includes('calculator')) {
         window.open('Calculator:///');
         const finalText = "Opening Calculator";
+        speak(finalText);
+    } else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what I found on the internet regarding " + message;
         speak(finalText);
     } else {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
